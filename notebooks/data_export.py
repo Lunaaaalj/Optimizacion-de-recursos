6a7ef7d08fa4e1862bc0170c322479f8b2a5ae04
Edward Luna <A00842657@tec.mx>
2026-02-26 08:42:45 -0600

    quitar indices inesesarios

--- notebooks/data_export.py
+++ notebooks/data_export.py
@@ -2,4 +2,9 @@
 import pandas as pd
 df = pd.read_excel("data/raw/Inventario bodega.xlsx",header = 2)
 print(df.head(5))
-df.to_csv("data/processed/inventario_bodega.csv")
+df.to_csv("data/processed/inventario_bodega.csv",index = False)
+
+# %%
+df = pd.read_excel("data/raw/beneficiarios 2025 (2).xlsx")
+print(df.head(10))
+df.to_csv("data/processed/beneficiarios.csv",index = False)
